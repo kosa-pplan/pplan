bacd3c7f505207073af7faa5bd8017f5d9107366
woodongle <112540904+woodongle@users.noreply.github.com>
2024-08-08 12:19:27 +0900

    Add files via upload

---
+++ Backend/src/main/java/org/pplan/controller/member/MemberRestController.java
@@ -0,0 +1,9 @@
+package org.pplan.controller.member;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MemberRestController {
+}
---
+++ Backend/src/main/java/org/pplan/service/MemberService.java
@@ -0,0 +1,7 @@
+package org.pplan.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
