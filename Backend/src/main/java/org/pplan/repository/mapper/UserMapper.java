a29f0bfaf405caa676991a6dcf21ddda8d18e3d8
woodongle <315ehdgur@gmail.com>
2024-08-08 17:39:53 +0900

    signup

---
+++ Backend/src/main/java/org/pplan/repository/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package org.pplan.repository.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.pplan.service.dto.User.UserDTO;
+
+@Mapper
+public interface UserMapper {
+
+    void insertUser(UserDTO userDTO);
+    UserDTO findByEmail(String email);
+}
