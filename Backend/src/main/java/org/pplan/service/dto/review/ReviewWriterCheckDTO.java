0b157bb03861a445656aebf79f9d2e20b018634a
gaengmin <momo5736@naver.com>
2024-08-13 10:10:24 +0900

    Fix 및 Feature : 리뷰 관련 오류 접근권한 설정 및 작성한 코드 주석 설명

---
+++ Backend/src/main/java/org/pplan/service/dto/review/ReviewWriterCheckDTO.java
@@ -0,0 +1,12 @@
+package org.pplan.service.dto.review;
+
+import lombok.Data;
+
+/**
+ * @Author KyeongMin
+ */
+@Data
+public class ReviewWriterCheckDTO {
+    String userEmail;
+    String reviewCheck;
+}
